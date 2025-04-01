2354223419b0587d9a3ec242c11bd234d1bd74b6
navideyar <nvd.sfy@gmail.com>
2025-04-01 14:12:46 +0330

    redirect to succes page for contact

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ app_name = 'contact'
 
 urlpatterns = [
      path('', views.contact, name='contact'),
+     path('success/', views.contact_success, name='contact_success'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('contact_success')
+            try:
+                form.save()
+                return redirect('contact:contact_success')
+            except Exception as e:
+                messages.error(request, f"خطا در ذخیره‌سازی فرم: {str(e)}")
+        else:
+            # نمایش خطاهای فرم
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"خطا در فیلد {field}: {error}")
     else:
         form = ContactForm()
     return render(request, 'contact-me.html', {'form': form})
+
+def contact_success(request):
+    return render(request, 'contact-success.html')

0cba997c71f58a80e713fb405adfb136e2286a3d
navideyar <nvd.sfy@gmail.com>
2025-04-06 12:37:20 +0330

    imorpve settings 4

--- cv2/wsgi.py
+++ cv2/wsgi.py
@@ -8,20 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-print("wsgi.py is being executed!", flush=True)
-print(f"Python version: {sys.version}", flush=True)
-print(f"Current working directory: {os.getcwd()}", flush=True)
 
 from django.core.wsgi import get_wsgi_application
 
 # اصلاح مسیر ماژول تنظیمات برای استفاده مستقیم از فایل تنظیمات تولید
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cv2.setting.production")
 
-print(f"DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}", flush=True)
-print(f"DJANGO_ENVIRONMENT: {os.environ.get('DJANGO_ENVIRONMENT')}", flush=True)
-
 application = get_wsgi_application()
-
-print("wsgi.py execution completed - application initialized", flush=True)

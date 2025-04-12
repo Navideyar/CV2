3850dc2de9956977d458b98efe158d030f009ed9
navideyar <nvd.sfy@gmail.com>
2025-04-12 20:13:53 +0330

    bug fix

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ app_name = 'main_core'
 urlpatterns = [
     path("", views.index, name="index"),
     path('policy/', views.policy, name='policy'),
+    path('policy-and-privacy/', views.policy, name='policy'),
     path('about-me/', views.about, name='about' ),
     path('personal-website/', views.personal_website, name='personal_website'),
     path('corporate-website/', views.corporate_website, name='corporate_website'),
